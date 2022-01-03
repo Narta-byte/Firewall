9d355a0c55fe736cfb444f576f79808a084eb4fb
Narta_byte <s204166@student.dtu.dk>
2022-01-03 15:28:59 +0100

    add some sim test

--- Firewall simulation/test.vhd
+++ Firewall simulation/test.vhd
@@ -1,4 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
 
-entity test0 is
-end entity;
+
+entity test is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic
+        
+    );
+end entity test;
+                
+architecture rtl of test is
+    signal clk_test : std_logic;
+    
+begin
+ process 
+ begin
+ clk_test <= '1';
+ wait for 10 ns;
+ clk_test <= '0';
+ wait for 10 ns;    
+ end process;
+    
+                
+end architecture;        
+
+
+
+
+
 
