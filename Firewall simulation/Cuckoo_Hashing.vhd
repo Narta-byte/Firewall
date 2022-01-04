05801478b04052d8ef55c99bfef8aad886fd36fa
s204188 <s204188>
2022-01-04 08:50:14 +0100

    added cuckoo hashing TB

---
+++ Firewall simulation/Cuckoo_Hashing.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+
+entity Cuckoo_Hashing is
+  port (
+    
+  ) ;
+end Cuckoo_Hashing ;
+
+architecture Cuckoo_Hashing_tb of Cuckoo_Hashing is
+
+
+
+begin
+
+
+
+end architecture ; -- Cuckoo_Hashing
---
+++ Firewall simulation/Cuckoo_Hashing_tb.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity Cuckoo_Hashing_tb is
+end Cuckoo_Hashing_tb ;
+
+architecture Cuckoo_Hashing_arch of Cuckoo_Hashing_tb is
+
+
+
+begin
+
+
+
+end architecture ; -- Cuckoo_Hashing_arch
