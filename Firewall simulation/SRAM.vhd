3d87a17b6f59a2205d13f308dbc4dabfef08cd08
unknown <s204166@student.dtu.dk>
2022-01-04 13:26:52 +0100

    added a SRAM

--- Firewall simulation/SRAM.vhd
+++ Firewall simulation/SRAM.vhd
@@ -7,19 +7,29 @@ entity SRAM is
   port (
     clk : in std_logic;
     reset : in std_logic;
-    RW : in std_logic;
+    WE : in std_logic; -- read/write
     address : in std_logic_vector(7 downto 0);
-    data_in : in std_logic_vector(9 downto 0);
-    data_out : out std_logic_vector(9 downto 0) 
+    data_in : in std_logic_vector(7 downto 0);
+    data_out : out std_logic_vector(7 downto 0) 
   ) ;
 end SRAM ;
 
 architecture SRAM_arch of SRAM is
   
-  
+  type RW_type is array (0 to 10) of std_logic_vector(7 downto 0);
+  signal RW : RW_type;
 
 begin
-
+  MEMORY : process (clk)
+  begin
+    if rising_edge(clk) then
+      if WE = '1' then
+        RW(to_integer(unsigned(address))) <= data_in;
+      else
+      data_out <= RW(to_integer(unsigned(address)));
+      end if ;
+    end if ;
+  end process;
 
 
 end architecture ; -- SRAM_arch
