0872dc363a9832a864ec5cd54e2a9b7fd00f92ad
s204188 <s204188>
2022-01-04 13:03:25 +0100

    Done with lunch

--- Firewall simulation/Cuckoo_Hashing.vhd
+++ Firewall simulation/Cuckoo_Hashing.vhd
@@ -2,19 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
-
+use ieee.std_logic_unsigned.ALL;
 
 entity Cuckoo_Hashing is
   port (
     clk : in std_logic;
     reset : in std_logic;
+    rule_flag : in std_logic;
     header_in : in std_logic_vector(95 downto 0);
-    hash_out : out std_logic_vector(95 downto 0)  
+    hash_out : out std_logic_vector(95 downto 0)
   ) ;
-end Cuckoo_Hashing ;
+end Cuckoo_Hashing ;grfasdydgidfsigluiafsgugfguaguugeirudsgugufghdyfasdgdygrdgfudfsfugudshiæyluikjwqhiydufjaklrkpiaufdygkjwrhetgiuf8syd7rhkt4jl5yhtub8f7dtfygithyjipth7g89tfyig4h56uyr7g86ftiu4h6i5puy98rg7f67iuoiup59y8rg779tu4o6iu59y8e7g6tutiup
 
 architecture Cuckoo_Hashing_tb of Cuckoo_Hashing is
 
+    type hash_table0 is array (0 to 10) of std_logic_vector(95 downto 0);
+    type hash_table1 is array (0 to 10) of std_logic_vector(95 downto 0);
+    
+    
     type State_type is (rule_searching, hash_matching );
     signal current_state, next_state : State_type;
 
@@ -36,11 +41,15 @@ begin
             if rising_edge(clk) then
                 case(current_state) is
                 
-                    when rule_searching =>
+                    when rule_searching => if rule_flag = '0' then
+                        next_state <= hash_matching;
+                    end if ;
                         
-                    when hash_matching =>
+                    when hash_matching => if rule_flag = '1' then
+                        next_state <= rule_searching;
+                    end if ;
 
-                    when others =>
+                    when others => next_state <= hash_matching;
                 
                 end case ;
             end if ;
@@ -49,15 +58,16 @@ begin
     OUTPUT_LOGIC : process (clk, reset)
     begin
         if reset = '1' then
-        
+            
             elsif rising_edge(clk) then
                 case(current_state) is
                 
-                    when rule_searching =>
-                        
+                    when rule_searching => 
+                    --hash_table0(1) <=  x"000000000000000000000000" ;
+
                     when hash_matching =>
 
-                    when others =>
+                    when others => report "ERROR IN OUTPUT LOGIC" severity failure;
                 
                 end case ;
             end if ;
---
+++ Firewall simulation/SRAM.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity SRAM is
+  port (
+    clock
+  ) ;
+end SRAM ;
+
+architecture SRAM_arch of SRAM is
+
+
+
+begin
+
+
+
+end architecture ; -- SRAM_arch
