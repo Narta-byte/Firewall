6de37b11bf4f8b3b9ae6a6984b25ff3979a5b8eb
Asger Bardrum <asgerbardrum@mbptilhndeasger.students.clients.local>
2022-01-05 15:49:41 +0100

    Might be done with the Collect_Header

--- Firewall simulation/Collect_Header.vhd
+++ Firewall simulation/Collect_Header.vhd
@@ -1,5 +1,3 @@
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -12,27 +10,65 @@ entity Collect_Header is
     packet_in : in std_logic_vector (7 downto 0);
     SoP : in std_logic;
     EoP : in std_logic;
-    ready : in std_logic;
+    vld : in std_logic;
+    ready_FIFO : in std_logic;
+    ready_hash : in std_logic;
 
-    header_in : out std_logic_vector (95 downto 0);
+    ready_hdr : out std_logic;
+    header_data : out std_logic_vector (95 downto 0);
     packet_forward : out std_logic_vector (7 downto 0);
+    vld_FIFO : out std_logic;
+    vld_hash : out std_logic
 
   );
 end entity;
 
 architecture Collect_Header_arch of Collect_Header is
   -- signal declarations
-  signal adressports : std_logic_vector (63 downto 0) := x"0000000000000000"; --fang adresseports her 
-  signal ports : std_logic_vector (31 downto 0) := x"00000000"; -- fang source ports her
+  signal srcaddr : std_logic_vector (31 downto 0) := x"00000000"; --Store source address here
+  signal destaddr : std_logic_vector (31 downto 0) := x"00000000"; --Store destination address here
+  signal srcport : std_logic_vector (15 downto 0) := x"00000000"; -- fang source ports her
+  signal destport : std_logic_vector (15 downto 0) := x"00000000"; -- fang destsource ports her
+  signal iter : integer := 0;
 
 begin
 
   Collect : process (clk, reset)
   begin
     if reset = '1' then
-      adressports <= x"0000000000000000";
-      ports <= x"00000000";
+      srcaddr <= x"00000000";
+      destaddr <= x"00000000";
+      srcport <= x"00000000";
+      destport <= x"00000000";
+      iter <= 0;
     elsif Rising_edge(clock) then
+      if Sop = '1' then
+        iter = 0;
+      end if;
+      iter <= iter + 1;
+
+      if iter >= 12 and iter <= 16 then
+        srcaddr <= srcaddr & packet_in;
+      end if;
+
+      if iter >= 17 and iter <= 21
+        destaddr <= desaddr & packet_in;
+      end if
+
+      if iter = 22 and iter = 23 then
+        srcport <= srcport & packet_in;
+      end if;
+
+      if iter = 24 and iter = 25 then
+        destport <= destport & packet_in;
+      end if;
+
+      header_data <= srcaddr & header_data;
+      header_data <= destaddr & header_data;
+      header_data <= srcport & header_data;
+      header_data <= destport & header_data;
+
+      packet_in <= packet_forward;
 
     end if;
 
---
+++ Firewall simulation/Collect_Header_TB.vhd
@@ -0,0 +1,96 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity Collect_Header_TB is
+end entity;
+
+architecture Collect_Header_TB_arch of Collect_Header_TB is
+
+  component Collect_Header is
+    port (
+      clk : in std_logic;
+      reset : in std_logic;
+      packet_in : in std_logic_vector (7 downto 0);
+      SoP : in std_logic;
+      EoP : in std_logic;
+      vld : in std_logic;
+      ready_FIFO : in std_logic;
+      ready_hash : in std_logic;
+
+      ready_hdr : out std_logic;
+      header_data : out std_logic_vector (95 downto 0);
+      packet_forward : out std_logic_vector (7 downto 0);
+      vld_FIFO : out std_logic;
+      vld_hash : out std_logic
+
+    );
+  end component;
+
+  -- signal declarations
+  signal clk_TB : std_logic;
+  signal reset_TB : std_logic;
+  signal packet_in_TB : std_logic_vector (7 downto 0);
+  signal SoP_TB : std_logic;
+  signal EoP_TB : std_logic;
+  signal vld_TB : std_logic;
+  signal ready_FIFO : std_logic;
+  signal ready_hash : std_logic;
+  signal ready_hash : std_logic;
+
+  signal ready_hdr : std_logic;
+  signal header_data : std_logic_vector (95 downto 0);
+  signal packet_forward : std_logic_vector (7 downto 0);
+  signal vld_FIFO : std_logic;
+  signal vld_hash : std_logic
+
+begin
+
+  DUT : Collect_Header port map(
+    clk_TB,
+    reset_TB,
+    packet_in_TB,
+    SoP_TB,
+    EoP_TB,
+    vld_TB,
+    ready_FIFO,
+    ready_hash,
+    ready_hash
+
+  );
+
+  Resetten : process
+  begin
+    reset_TB <= '1';
+    wait for 1ns;
+    reset_TB <= '0';
+    wait;
+  end process;
+
+  Clocken : process
+  begin
+    clk_TB <= '1';
+    wait for 1ns;
+    clk_TB <= '0';
+    wait for 0ns;
+  end process;
+
+  logic : process (clk_TB, reset_TB)
+  begin
+    if reset_TB = '1' then
+      -- set all to 0
+      packet_in <= x"00";
+      SoP <= '0';
+      EoP <= '0';
+      vld <= '0';
+      ready_FIFO <= '0';
+      ready_hash <= '0';
+
+    elsif Rising_Edge(clk_TB) then
+      -- Logics
+
+    end if;
+
+  end process;
+end architecture;
