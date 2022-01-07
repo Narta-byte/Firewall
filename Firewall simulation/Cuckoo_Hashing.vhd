a7b92d7b8b8f448e79e8d0de133935f5c20a755c
unknown <s204166@student.dtu.dk>
2022-01-07 08:58:41 +0100

    worked on cuckoo hashing, it almost works

--- Firewall simulation/Cuckoo_Hashing.vhd
+++ Firewall simulation/Cuckoo_Hashing.vhd
@@ -1,8 +1,13 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
-use ieee.std_logic_unsigned.ALL;
+library IEEE;
+library std;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
+
+
 
 entity Cuckoo_Hashing is
   port (
@@ -14,25 +19,33 @@ entity Cuckoo_Hashing is
     key_in : in std_logic_vector(95 downto 0);
 
     header_in : in std_logic_vector(95 downto 0);
-    
-    
+
+
+    val : in std_logic;
+    rdy : out std_logic;
 
     acc_deny_out : out std_logic
   ) ;
 end Cuckoo_Hashing ;
 
 architecture Cuckoo_Hashing_tb of Cuckoo_Hashing is
-   
+
     type State_type is (rule_searching, hash_matching, lookup_hash1,lookup_hash2,insert_key,remember_and_replace
-,ERROR,is_occupied);
+,ERROR,is_occupied,rdy_key);
     signal current_state, next_state : State_type;
-	
+
     signal RW : std_logic:='0';
 
 
     signal exits,insert_flag,hashfun,flip : std_logic := '0';
     signal MAX : integer:= 0;
     signal old_key : std_logic_vector(95 downto 0);
+    signal rdy_interal : std_logic;
+    
+    signal test_int : integer;
+    signal test_old_key : std_logic_vector(95 downto 0);
+    signal test_sram_msb : std_logic := '0';
+    
     
 
     component SRAM is
@@ -44,18 +57,22 @@ architecture Cuckoo_Hashing_tb of Cuckoo_Hashing is
             occupied_flag_in : in std_logic;
             hash_in : in std_logic_vector(5 downto 0);
             key_in : in std_logic_vector(95 downto 0);
-            data_out : out std_logic_vector(20 downto 0) 
+            data_out : out std_logic_vector(102 downto 0)
         );
     end component;
-   signal hash_out : std_logic_vector(7 downto 0);
-   signal key_out :  std_logic_vector(5 downto 0);
+   signal hash_out : std_logic_vector(5 downto 0);
+   signal key_out :  std_logic_vector(95 downto 0);
    signal address :  std_logic_vector(5 downto 0);
    signal occupied_flag_out :  std_logic;
-   signal SRAM_data :  std_logic_vector(13 downto 0);
+   signal SRAM_data :  std_logic_vector(102 downto 0);
+
+   signal insertion_key : std_logic_vector(95 downto 0);
+   
+   
 begin
 
-    SRAM_in : SRAM port map (clk,reset,RW,hash_out,occupied_flag_out,key_out,SRAM_data);
-
+    SRAM_in : SRAM port map (clk,reset,RW,address,occupied_flag_out,hash_out,key_out,SRAM_data);
+    rdy <= rdy_interal;
 
     STATE_MEMORY_LOGIC : process (clk, reset)
     begin
@@ -65,20 +82,23 @@ begin
             current_state <= next_state;
         end if ;
     end process;
-    
-    NEXT_STATE_LOGIC : process (current_state)
+
+    NEXT_STATE_LOGIC : process (current_state, insert_flag, set_rule, rdy_interal, val, exits, max, flip)
     begin
         next_state <= current_state;
-        
-            if rising_edge(clk) then
+
+
                 case(current_state) is
-                
+
                     when rule_searching => if insert_flag ='1' then
-                        next_state <= lookup_hash1;
+                        next_state <= rdy_key;
                     elsif set_rule = '0' then
                         next_state <= hash_matching;
                     end if ;
-                        
+                    when rdy_key => if rdy_interal = '1' and val ='1' then
+                        next_state <= lookup_hash1;
+                    end if ;
+
                     when hash_matching => if set_rule = '1' then
                         next_state <= rule_searching;
                     end if ;
@@ -90,7 +110,7 @@ begin
                     else
                         next_state <= insert_key;
                     end if;
-                    
+
                     when lookup_hash2 => next_state <= is_occupied;
                     when remember_and_replace => if max = 16 then
                         next_state <= ERROR;
@@ -99,73 +119,85 @@ begin
                     else
                         next_state <= lookup_hash2;
                     end if;
-                    when insert_key => next_state <= rule_searching;
+                    when insert_key => next_state <= rdy_key;
                     when others => next_state <= hash_matching;
-                
+
                 end case ;
-            end if ;
+
     end process;
 
-    OUTPUT_LOGIC : process (clk, reset)
+    OUTPUT_LOGIC : process (current_state, reset, cmd_in)
     begin
-        if reset = '1' then
-            
-            elsif rising_edge(clk) then
+
                 case(current_state) is
-                
-                    when rule_searching => 
+
+                    when rule_searching =>
                     case( cmd_in ) is
-                   
+
                        when "00" => --flush
                            --TODO
-                       when "01" => --insert 
+                       when "01" => --insert
                             insert_flag <= '1';
                        when "10" => -- delete
                            --TODO
                        when others => Report "CMD CANNOT BE 11" severity NOTE;
-                   
+
                    end case ;
                        when hash_matching =>
-            
+
                     when insert_key =>
                         if hashfun = '0' then
-                            address <= to_integer(unsigned(key_in)) mod 11;
+                            address <=    std_logic_vector(to_unsigned(to_integer(unsigned(insertion_key)) mod 11,96)(5 downto 0)) ;
                             RW <= '1';
                             occupied_flag_out <= '1';
-                            hash_out <= key_in mod 11;
-                            key_out  <= key_in;
+                            --hash_out <=std_logic_vector(to_unsigned(to_integer(unsigned(key_in)) mod 11,96)(5 downto 0));
+                            hash_out <= "000000";
+                            --key_out  <= key_in;
+                            key_out <= insertion_key;
+                            flip <= '1';
                         else
-                            address <= key_in/11 mod 11+15; -- an abritary number
+                            address <= std_logic_vector(to_unsigned(to_integer(unsigned(insertion_key))/11 mod 11+15,96)(5 downto 0)); -- an abritary number
                             RW <= '1';
                             occupied_flag_out <= '1';
-                            hash_out <= key_in/11 mod 11;
-                            key_out  <= key_in;
+                            --hash_out <= std_logic_vector(to_unsigned(to_integer(unsigned(key_in))/11 mod 11+15,96)(5 downto 0));
+                            hash_out <= "000000";
+                            --key_out  <= key_in;
+                            key_out <= insertion_key;
 
                         end if;
-                       
+                    when rdy_key => rdy_interal <= '1';
+
                     when lookup_hash1 =>
+                        rdy_interal <= '0';
                         hashfun <= '0';
                         RW <= '0';
-                        address <= key_in mod 11;
-                        
+                        address <= std_logic_vector(to_unsigned(to_integer(unsigned(insertion_key)) mod 11,96)(5 downto 0));
+
                     when lookup_hash2 =>
                         hashfun <= '1';
                         RW <= '0';
-                        address <= key_in/11 mod 11;
-                        
+                        address <= std_logic_vector(to_unsigned(to_integer(unsigned(insertion_key))/11 mod 11,96)(5 downto 0));
+
                     when is_occupied =>
-                        if SRAM_data(101) = '1' then
+                        test_sram_msb <= SRAM_data(102);
+
+                        insertion_key <= key_in;
+                        if SRAM_data(102) = '1' then
                             exits <= '1';
                         else
                             exits <= '0';
                         end if;
-                        
+
                     when remember_and_replace =>
                         
                         old_key <= SRAM_data(95 downto 0);
                         RW <= '1';
-                        key_out <= key_in;
-                        hash_out <= key_in mod 11;
+                        --key_out <= key_in;
+                        key_out <= insertion_key;
+                        insertion_key <= SRAM_data(95 downto 0);
+                        --hash_out <= std_logic_vector(to_unsigned(to_integer(unsigned(key_in)) mod 11,96)(5 downto 0));
+                        hash_out <= "000000";
+                        occupied_flag_out <= '1'; --skal måske være her
                         if flip = '1' then
                             flip <= '0';
                         else
@@ -174,13 +206,11 @@ begin
                     when ERROR =>
 
                     when others => report "ERROR IN OUTPUT LOGIC" severity failure;
-                
+
                 end case ;
-            end if ;
+
     end process;
 
-
-    
 
 end architecture ; -- Cuckoo_Hashing
 
--- Firewall simulation/Cuckoo_Hashing_tb.vhd
+++ Firewall simulation/Cuckoo_Hashing_tb.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
 
 entity Cuckoo_Hashing_tb is
 end;
@@ -15,19 +17,11 @@ architecture bench of Cuckoo_Hashing_tb is
       cmd_in : in std_logic_vector(1 downto 0);
       key_in : in std_logic_vector(95 downto 0);
       header_in : in std_logic_vector(95 downto 0);
-      address : out std_logic_vector(5 downto 0);
-      RW : out std_logic;
-      hash_out : out std_logic_vector(7 downto 0);
-      key_out : out std_logic_vector(8 downto 0);
-      occupied_flag_out : out std_logic;
-      SRAM_data : in std_logic_vector(13 downto 0);
+      val : in std_logic;
+      rdy : out std_logic;
       acc_deny_out : out std_logic
     );
   end component;
-
-  -- Clock period
-  constant clk_period : time := 5 ns;
-  -- Generics
 
   -- Ports
   signal clk : std_logic;
@@ -36,13 +30,20 @@ architecture bench of Cuckoo_Hashing_tb is
   signal cmd_in : std_logic_vector(1 downto 0);
   signal key_in : std_logic_vector(95 downto 0);
   signal header_in : std_logic_vector(95 downto 0);
-  signal address : std_logic_vector(5 downto 0);
-  signal RW : std_logic;
-  signal hash_out : std_logic_vector(7 downto 0);
-  signal key_out : std_logic_vector(8 downto 0);
-  signal occupied_flag_out : std_logic;
-  signal SRAM_data : std_logic_vector(13 downto 0);
+  signal val : std_logic;
+  signal rdy : std_logic;
   signal acc_deny_out : std_logic;
+
+  -- fsm logic
+  type State_type is (setup_rulesearch,set_key,wait_for_ready,send_key,TERMINATE);
+  signal current_state, next_state : State_type;
+
+  signal data_end,done_looping : std_logic :='0';
+
+  --maybe make theese varibles in output logic  
+  signal cnt : integer;
+  type data_array is array (0 to 9) of std_logic_vector(7 downto 0);
+  signal data_array_sig : data_array;
 
 begin
 
@@ -54,29 +55,103 @@ begin
       cmd_in => cmd_in,
       key_in => key_in,
       header_in => header_in,
-      address => address,
-      RW => RW,
-      hash_out => hash_out,
-      key_out => key_out,
-      occupied_flag_out => occupied_flag_out,
-      SRAM_data => SRAM_data,
+      val => val,
+      rdy => rdy,
       acc_deny_out => acc_deny_out
     );
 
-    process 
-    begin
-        clk <= '1';
-        wait for 10 ns;
-        clk <= '0';
-        wait for 10 ns;
-        
+CLK_PROCESS : process 
+begin
+  clk <= '1';
+  wait for 10 ns;
+  clk <= '0';
+    wait for 10 ns;
     end process;
---   clk_process : process
---   begin
---   clk <= '1';
---   wait for clk_period/2;
---   clk <= '0';
---   wait for clk_period/2;
---   end process clk_process;
 
-end;
+
+    STATE_MEMORY_LOGIC : process (clk, reset)
+    begin
+        if reset = '1' then
+            current_state <= setup_rulesearch;
+        elsif rising_edge(clk) then
+            current_state <= next_state;
+        end if ;
+    end process;  
+
+    --(setup_rulesearch,set_key,wait_for_ready,send_key,TERMINATE);
+
+  NEXT_STATE_LOGIC : process (current_state, done_looping, rdy, val, data_end)
+  begin    
+      case current_state is
+        when setup_rulesearch =>
+          next_state <= set_key;
+  
+        when set_key => if done_looping = '1' then
+          next_state <= wait_for_ready;
+        end if ;
+
+        when wait_for_ready => if rdy = '1' and val = '1' then
+          next_state <= send_key;
+        elsif data_end = '1' then
+          next_state <= TERMINATE;
+        end if ;
+        
+        when send_key =>
+          next_state <= wait_for_ready;
+        
+        when TERMINATE => 
+          next_state <= setup_rulesearch;
+      
+        when others =>
+          next_state <= setup_rulesearch;
+      end case;
+  end process;
+
+  OUTPUT_LOGIC : process (clk,reset)
+  file input : TEXT open READ_MODE is "cuckoo_hash testdata.txt";
+  variable current_read_line : line;
+  variable hex_reader : std_logic_vector(7 downto 0);
+  
+  begin
+    if reset = '1' then
+    
+    elsif rising_edge(clk) then
+      
+      case current_state is
+
+      when setup_rulesearch => 
+        set_rule <= '1';
+	      cnt <= 0;
+
+      when set_key =>
+       
+          READ_ARRAY : for i in 0 to 9 loop
+            if not ENDFILE(input) then
+              
+              readline(input, current_read_line);
+              HREAD(current_read_line, hex_reader);
+              
+              data_array_sig(i) <= hex_reader;
+              end if ;
+          
+          end loop ; -- READ_ARRAY
+
+
+        done_looping <= '1';
+        cmd_in <= "01";
+        val <= '1';
+      when wait_for_ready => 
+            
+      when send_key =>
+          key_in <= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" & data_array_sig(cnt);
+          cnt <= cnt+1;
+      when TERMINATE => 
+          
+      when others => report "FAILURE" severity failure;
+          
+      end case;
+    end if;
+  end process;
+
+
+ end;
