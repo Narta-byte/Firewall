2af9186813ed9d36b965e9424a17bc289f12a541
unknown <s204166@student.dtu.dk>
2022-01-13 08:42:36 +0100

    fixed cuckoo hash tb

--- Firewall simulation/Cuckoo_Hashing_tb.vhd
+++ Firewall simulation/Cuckoo_Hashing_tb.vhd
@@ -7,6 +7,10 @@ use std.textio.all;
 use STD.textio.all;
 use IEEE.std_logic_textio.all;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity Cuckoo_Hashing_tb is
 end;
 
@@ -19,18 +23,18 @@ architecture bench of Cuckoo_Hashing_tb is
       set_rule : in std_logic;
       cmd_in : in std_logic_vector(1 downto 0);
       key_in : in std_logic_vector(95 downto 0);
-      header_in : in std_logic_vector(95 downto 0);
-      val_hdr : in std_logic;
-      rdy_hdr : out std_logic;
-      val_in : in std_logic;
-      rdy_out : out std_logic;
+      header_data : in std_logic_vector(95 downto 0);
+      vld_hdr : in std_logic;
+      rdy_hash : out std_logic;
+      vld_firewall_hash : in std_logic;
+      rdy_firewall_hash : out std_logic;
       acc_deny_out : out std_logic;
-      val_ad : out std_logic;
-      rdy_ad : in std_logic
+      vld_ad_hash : out std_logic;
+      rdy_ad_hash : in std_logic
     );
   end component;
 
-
+  
 
   -- Ports
   signal clk : std_logic;
@@ -38,15 +42,16 @@ architecture bench of Cuckoo_Hashing_tb is
   signal set_rule : std_logic;
   signal cmd_in : std_logic_vector(1 downto 0);
   signal key_in : std_logic_vector(95 downto 0);
-  signal header_in : std_logic_vector(95 downto 0);
-  signal val_hdr : std_logic;
-  signal rdy_hdr : std_logic;
-  signal val_in : std_logic;
-  signal rdy_out : std_logic;
+  signal header_data : std_logic_vector(95 downto 0);
+  signal vld_hdr : std_logic;
+  signal rdy_hash : std_logic;
+  signal vld_firewall_hash : std_logic;
+  signal rdy_firewall_hash : std_logic;
   signal acc_deny_out : std_logic;
-  signal val_ad : std_logic;
-  signal rdy_ad : std_logic;
+  signal vld_ad_hash : std_logic;
+  signal rdy_ad_hash : std_logic;
 
+ 
 
   -- fsm logic
   type State_type is (setup_rulesearch,set_key,wait_for_ready_insert,send_key,terminate_insertion, wipe_memory,wait_for_last_calc_to_finish,
@@ -70,22 +75,23 @@ architecture bench of Cuckoo_Hashing_tb is
   
 begin
 
-    Cuckoo_Hashing_inst : Cuckoo_Hashing
-      port map (
-        clk => clk,
-        reset => reset,
-        set_rule => set_rule,
-        cmd_in => cmd_in,
-        key_in => key_in,
-        header_in => header_in,
-        val_hdr => val_hdr,
-        rdy_hdr => rdy_hdr,
-        val_in => val_in,
-        rdy_out => rdy_out,
-        acc_deny_out => acc_deny_out,
-        val_ad => val_ad,
-        rdy_ad => rdy_ad
-      );
+  Cuckoo_Hashing_inst : Cuckoo_Hashing
+  port map (
+    clk => clk,
+    reset => reset,
+    set_rule => set_rule,
+    cmd_in => cmd_in,
+    key_in => key_in,
+    header_data => header_data,
+    vld_hdr => vld_hdr,
+    rdy_hash => rdy_hash,
+    vld_firewall_hash => vld_firewall_hash,
+    rdy_firewall_hash => rdy_firewall_hash,
+    acc_deny_out => acc_deny_out,
+    vld_ad_hash => vld_ad_hash,
+    rdy_ad_hash => rdy_ad_hash
+  );
+
   
  
 
@@ -108,8 +114,8 @@ begin
     end process;  
 
 
-  NEXT_STATE_LOGIC : process (current_state, done_looping, rdy_out, val_in, data_end,calc_is_done,
-                              rdy_hdr, val_hdr, cnt_calc_fin, rdy_ad)
+  NEXT_STATE_LOGIC : process (current_state, done_looping, rdy_firewall_hash, vld_firewall_hash, data_end,calc_is_done,
+                              rdy_hash, vld_hdr, cnt_calc_fin, rdy_ad_hash)
   begin
     next_state <= current_state; -- måske sus
       case current_state is
@@ -125,7 +131,7 @@ begin
         next_state <= send_key;
         when wait_for_ready_insert => if data_end = '1' then
           next_state <= wait_for_last_calc_to_finish;
-        elsif  rdy_out = '1' and val_in = '1'then
+        elsif  rdy_firewall_hash = '1' and vld_firewall_hash = '1'then
           next_state <= send_key;
         end if ;
         
@@ -133,14 +139,14 @@ begin
           next_state <= wait_for_ready_insert;
         
         when terminate_insertion => 
-          if not (cnt_calc_fin = 2) and rdy_out = '1' then
+          if not (cnt_calc_fin = 2) and rdy_firewall_hash = '1' then
             cnt_calc_fin <= cnt_calc_fin +1;
           elsif (cnt_calc_fin = 2)  then
             next_state <= goto_cmd_state;
           end if;
           
          when wait_for_last_calc_to_finish => 
-          if rdy_out = '1' then
+          if rdy_firewall_hash = '1' then
             next_state <= terminate_insertion;
           end if;
 
@@ -156,7 +162,7 @@ begin
         when wait_for_ready_match => 
           if match_done = '1' then
             next_state <= wait_for_match_to_fin;
-          elsif  (rdy_hdr = '1') and (val_hdr = '1')  then
+          elsif  (rdy_hash = '1') and (vld_hdr = '1')  then
             next_state <= send_match_key;
           end if ;
         when terminate_match => next_state <= terminate_match;
@@ -164,12 +170,12 @@ begin
         when test_a_wrong_header => next_state <= wait_for_wrong_calc_to_fin;
             
         when wait_for_match_to_fin => 
-          if rdy_hdr = '1' then
+          if rdy_hash = '1' then
             next_state <= test_a_wrong_header;
           end if;
         
         when wait_for_wrong_calc_to_fin =>
-          if rdy_hdr = '1' then
+          if rdy_hash = '1' then
             next_state <= terminate_match;
           end if;
         when others =>
@@ -207,7 +213,7 @@ begin
 
         done_looping <= '1';
         cmd_in <= "01";
-        val_in <= '1';
+        vld_firewall_hash <= '1';
       when wait_for_ready_insert => 
             
       when send_key =>
@@ -222,31 +228,31 @@ begin
           --   writeline(output,write_line);
           -- end loop ; -- DEBUG_LOOP
       when terminate_insertion => 
-              val_in <= '0';
+              vld_firewall_hash <= '0';
               cmd_in <= "11";
 
-      when wait_for_last_calc_to_finish => --val_in <= '0';
+      when wait_for_last_calc_to_finish => --vld_firewall_hash <= '0';
       
       when goto_cmd_state => 
 
       when start_hash_matching => 
           cmd_in <= "11";
           cnt <= 0;
-          val_hdr <= '1';
-          rdy_ad <= '1'; --this simulates that the accept deny block is always ready
-          --header_in <= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" & data_array_sig(cnt); 
+          vld_hdr <= '1';
+          rdy_ad_hash <= '1'; --this simulates that the accept deny block is always ready
+          --header_data <= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" & data_array_sig(cnt); 
           
          
           
       when send_match_key => 
-          header_in <=  data_array_sig(cnt); 
+          header_data <=  data_array_sig(cnt); 
           cnt <= cnt +1;            
           if cnt = 160 then
             match_done <= '1';
           end if ;
 
       when wait_for_ready_match => 
-            --val_hdr<= '1';
+            --vld_hdr<= '1';
             set_rule <= '0';
           if acc_deny_out = '1' then
               ok_cnt <= ok_cnt +1;
@@ -255,10 +261,10 @@ begin
           end if;
             
 
-      when terminate_match => val_hdr <= '0';
+      when terminate_match => vld_hdr <= '0';
 
       when test_a_wrong_header => 
-        header_in <= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" & "00011111";
+        header_data <= "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" & "00011111";
 
       when wait_for_match_to_fin => 
       
