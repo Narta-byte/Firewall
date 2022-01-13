ec41793bc39e07d47664af98a8be6c18e34bbf0b
Cecilie Haslund Bertelsen <97030447+cecil7525@users.noreply.github.com>
2022-01-13 16:43:38 +0100

    AD not done

--- Firewall simulation/Accept_Deny.vhd
+++ Firewall simulation/Accept_Deny.vhd
@@ -1,7 +1,11 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
+library IEEE;
+library std;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
 
 entity Accept_Deny is
     port (
@@ -12,27 +16,27 @@ entity Accept_Deny is
         --vld_ad      : out std_logic; 
 
         -- FIFO interface 
-        packet_forward_FIFO    : in std_logic_vector(7 downto 0);
-        FIFO_sop    : in std_logic;
-        FIFO_eop    : in std_logic;
-        vld_fifo    : in std_logic;
+        packet_forward_FIFO     : in std_logic_vector(9 downto 0);
+        --fifo_sop                : in std_logic;
+        --fifo_eop                : in std_logic;
+        vld_fifo                : in std_logic;
 
         -- Cuckoo interface 
         acc_deny    : in std_logic;
         vld_hash    : in std_logic;
 
-        rdy_ad_h    : out std_logic := '1';
-        rdy_ad_FIFO    : out std_logic := '1';
-        data_firewall     : out std_logic_vector(7 downto 0);
-        out_sop     : out std_logic;
-        out_eop     : out std_logic;
-        ok_cnt      : out std_logic_vector;
-        ko_cnt      : out std_logic_vector
+        rdy_ad_hash      : out std_logic;
+        rdy_ad_FIFO      : out std_logic;
+        data_firewall    : out std_logic_vector(9 downto 0);
+        --out_sop          : out std_logic;
+        --out_eop          : out std_logic;
+        ok_cnt           : out std_logic_vector(8 downto 0);
+        ko_cnt           : out std_logic_vector(8 downto 0)
     );
 end entity;
 
 
-architecture rtl of Accept_Deny is
+architecture arch_Accept_Deny of Accept_Deny is
 
     -- Other signals here 
     type state_type is (wait_hash, get_packet, accept_and_forward, deny_and_delete);
@@ -108,7 +112,11 @@ begin
                                     next_state <= deny_and_delete;  
                                 end if;
 
-                        when accept_and_forward => next_state <= wait_hash;
+                        when accept_and_forward =>
+                                if eop = '1'then 
+                                    next_state <= wait_hash;
+                                else 
+                                    next_state <= accept_and_forward;
 
                         when deny_and_delete => next_state <= wait_hash; 
 
@@ -116,44 +124,38 @@ begin
         end process;
 
 
-        OUTPUT_LOGIC : process (current_state, vld_hash, full, empty, full_packet, acc_deny, vld_fifo)
+        OUTPUT_LOGIC : process (current_state, vld_hash, full, empty, acc_deny, vld_fifo)
         begin
 
                 case(current_state) is 
 
                     when wait_hash =>
-                        -- if vld_hash = '0' then 
-                        --     rdy_ad_f <= '0';
-                        -- elsif vld_hash = '1' then
-                        --     rdy_ad_f<= '1';
-                        -- end if;
-                        rdy_ad_h <= '1';
+                        rdy_ad_hash <= '1';
 
                     when get_packet =>
-                        if (full = '1') and (vld_fifo= '1') then 
-                            full_packet <= q;
-                            --rdy_ad_h <= '0';
-                        elsif  empty = '0' and (vld_fifo= '1') then 
+                        if (full = '1') and (vld_fifo = '1') then 
+                           data_firewall <= packet_forward_FIFO;
+                        elsif  (empty = '0') and (vld_fifo = '1') then 
                             report "FALIURE" severity failure; 
                         end if; 
-                        rdy_ad_h <= '0';
-                        rdy_ad_f <= '1';
+                        rdy_ad_hash <= '0';
+                        rdy_ad_FIFO <= '1';
 
                     when accept_and_forward => 
-                        if acc_deny = '1'  then 
-                            packet_out <= q;
-                            rdy_ad_h <= '0';
-                            rdy_ad_f <= '0'; 
+                        if (acc_deny = '1')  then 
+                            data_firewall <= packet_forward_FIFO;
+                            rdy_ad_hash <= '0';
+                            rdy_ad_FIFO <= '0'; 
                         end if;
-                        int_ok <= int_ok + 1;
+                            int_ok <= int_ok + 1;
 
                     when deny_and_delete => 
-                        if acc_deny = '0' then 
-                            packet_out <= (others => '0'); --"00000000" 
-                            rdy_ad_h <= '0';
-                            rdy_ad_f <= '0';  
+                        if (acc_deny = '0') then
+                           data_firewall <= (others => '0');
+                           rdy_ad_hash <= '0';
+                           rdy_ad_FIFO <= '0';
                         end if; 
-                        int_ko <= int_ko + 1;  
+                            int_ko <= int_ko + 1;
                     
                     when others => report "ERROR IN OUTPUT LOGIC" severity failure; 
                     
@@ -162,10 +164,7 @@ begin
             
         end process;
 
-        full_packet <=  dat_FIFO & FIFO_sop  & FIFO_eop;
-        packet_out <= q; 
         ok_cnt <= std_logic_vector(to_signed(int_ok, ok_cnt'length));
         ko_cnt <= std_logic_vector(to_signed(int_ko, ko_cnt'length));
         
-
 end architecture;
--- Firewall simulation/Accept_Deny_tb.vhd
+++ Firewall simulation/Accept_Deny_tb.vhd
@@ -1,103 +1,159 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+library std;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
 
 entity Accept_Deny_tb is
 end;
 
+
 architecture bench of Accept_Deny_tb is
 
+  --Decleration of Accept_Deny signals  
+  signal clk                 : std_logic;
+  signal reset               : std_logic;
+  signal data_firewall       : std_logic_vector(9 downto 0) := (others => '0');
+  --signal out_sop : std_logic;
+  --signal out_eop : std_logic;
+  signal ok_cnt              : std_logic_vector(8 downto 0):= (others => '0');
+  signal ko_cnt              : std_logic_vector(8 downto 0):= (others => '0');
+  signal packet_forward_FIFO : std_logic_vector(9 downto 0) := "1010101010";
+  --signal FIFO_sop : std_logic;
+  --signal FIFO_eop : std_logic;
+  signal vld_fifo : std_logic := '1';
+  signal acc_deny : std_logic := '0';
+  signal rdy_ad_FIFO : std_logic;
+  signal vld_hash : std_logic := '1';
+  signal rdy_ad_hash : std_logic;
+  
+  -- Component for Accept_Deny
   component Accept_Deny
-      port (
-      clk : in std_logic;
-      reset : in std_logic;
-      out_dat : out std_logic_vector(7 downto 0);
-      out_sop : out std_logic;
-      out_eop : out std_logic;
-      ok_cnt : out std_logic_vector;
-      ko_cnt : out std_logic_vector;
-      dat_FIFO : in std_logic_vector(7 downto 0);
-      FIFO_sop : in std_logic;
-      FIFO_eop : in std_logic;
-      vld_fifo : in std_logic;
-      rdy_ad_f : out std_logic;
-      acc_deny : in std_logic;
-      vld_hash : in std_logic;
-      rdy_ad_h : out std_logic
-    );
-  end component;
+    port (
+    clk                 : in std_logic; --yes
+    reset               : in std_logic; --yes
+    packet_forward_FIFO : in std_logic_vector(9 downto 0); --yes
+    --FIFO_sop : in std_logic; 
+    --FIFO_eop : in std_logic; 
+    vld_fifo            : in std_logic; --yes
+    acc_deny            : in std_logic; --yes
+    vld_hash            : in std_logic; --yes
+    rdy_ad_hash         : out std_logic; --yes
+    rdy_ad_FIFO         : out std_logic; -- micheal hjælp
+    data_firewall       : out std_logic_vector(9 downto 0); --yes
+    --out_sop : out std_logic; 
+    --out_eop : out std_logic;
+    ok_cnt               : out std_logic_vector(8 downto 0); --yes ændres måske senere
+    ko_cnt               : out std_logic_vector(8 downto 0) --yes ændres måske senere
+  );
+end component;
 
-  -- Other signals here 
-  type state_type is (wait_hash, get_packet, accept_and_forward, deny_and_delete);
-  signal current_state, next_state : state_type;
+--Other signals 
+type state_type is (do_nothing, packet_input, waiting);
+signal current_state, next_state : state_type;
 
-  -- Ports
-  signal clk : std_logic;
-  signal reset : std_logic;
-  signal out_dat : std_logic_vector(7 downto 0);
-  signal out_sop : std_logic;
-  signal out_eop : std_logic;
-  signal ok_cnt : std_logic_vector;
-  signal ko_cnt : std_logic_vector;
-  signal dat_FIFO : std_logic_vector(7 downto 0);
-  signal FIFO_sop : std_logic;
-  signal FIFO_eop : std_logic;
-  signal vld_fifo : std_logic;
-  signal rdy_ad_f : std_logic;
-  signal acc_deny : std_logic;
-  signal vld_hash : std_logic;
-  signal rdy_ad_h : std_logic;
+signal set_data : std_logic := '1'; 
+
 
 begin
 
-  Accept_Deny_inst : Accept_Deny
+  -- Portmapping
+  DUT : Accept_Deny
     port map (
       clk => clk,
       reset => reset,
-      out_dat => out_dat,
-      out_sop => out_sop,
-      out_eop => out_eop,
-      ok_cnt => ok_cnt,
-      ko_cnt => ko_cnt,
-      dat_FIFO => dat_FIFO,
-      FIFO_sop => FIFO_sop,
-      FIFO_eop => FIFO_eop,
+      packet_forward_FIFO => packet_forward_FIFO,
       vld_fifo => vld_fifo,
-      rdy_ad_f => rdy_ad_f,
       acc_deny => acc_deny,
       vld_hash => vld_hash,
-      rdy_ad_h => rdy_ad_h
-    );
+      rdy_ad_hash => rdy_ad_hash,
+      rdy_ad_FIFO => rdy_ad_FIFO,
+      data_firewall => data_firewall,
+      ok_cnt => ok_cnt,
+      ko_cnt => ko_cnt
+      );
 
-
+   --SUPER
    clk_process : process
        begin
            clk <= '1';
-       wait for 10 ns;
+       wait for 1 ns;
            clk <= '0';
-        wait for 10 ns;
-    end process clk_process;
+        wait for 1 ns;
+    end process;
 
-    
+    -- SUPER 
     STATE_MEMORY_LOGIC : process (clk, reset)
     begin
         if reset = '1' then
-            current_state <= wait_hash;
+            current_state <= do_nothing;
         elsif rising_edge(clk) then
             current_state <= next_state;
         end if ;
     end process;  
 
 
-    NEXT_STATE_LOGIC : process (all)
+    --PENDING 
+    NEXT_STATE_LOGIC : process (current_state, vld_fifo, vld_hash, set_data)
     begin
+        case current_state is
+          when do_nothing =>
+            if set_data = '0' then
+              next_state <= do_nothing;
+              
+            elsif vld_fifo = '1' and vld_hash = '1' then 
+              next_state <= packet_input;
+             
+            end if;
+          
+          when packet_input => 
+            if set_data = '0' then 
+              next_state <= do_nothing; 
+    
+            elsif vld_fifo = '0' and vld_hash = '0' then
+              next_state <= waiting;
         
+            elsif vld_fifo = '1' and vld_hash = '1' then 
+              next_state <= packet_input;
+     
+            end if;  
+
+          when waiting =>
+            if vld_fifo = '0' and vld_hash = '0' then
+              next_state <= waiting; 
+       
+            elsif vld_fifo = '1' and vld_hash = '1' then 
+              next_state <= packet_input;
+   
+            end if;
+
+          when others => next_state <= do_nothing;
+
+        end case;
     end process;
 
-
-    OUTPUT_LOGIC : process (all)
+    --PENDING
+    OUTPUT_LOGIC : process (clk)
+    
     begin
+      if Rising_Edge(clk) then 
+        case current_state is 
+          when do_nothing =>
+              -- Do Nothing 
+          
+          when packet_input => 
+
+              data_firewall <= packet_forward_FIFO;
         
+          when waiting => 
+              -- Waiting
+         
+          when others => report "FAILURE" severity failure;
+        end case;
+      end if;
     end process;
 
 end;
