b34cb872b3be2f8f6f84b4b7e5e6fbe0f30a861d
Asger Bardrum <asgerbardrum@mbptilhndeasger.students.clients.local>
2022-01-21 10:29:08 +0100

    java files are in

---
+++ Java scripts/oldscripts/src/deleteToROM.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class deleteToROM {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Scanner reader = new Scanner(new File("delete_keys.txt"));
+            FileWriter writer = new FileWriter(new File("output_delete_keys.txt"));
+            String str = "";
+            boolean first = true;
+            int i = 1;
+            System.out.println("test1");
+
+            int nmbOfLines = 0;
+            Scanner reader2 = new Scanner(new File("delete_keys.txt"));
+
+            while (reader2.hasNext()) {
+                reader2.next();
+                nmbOfLines++;
+            }
+            System.out.println(nmbOfLines);
+
+            while (reader.hasNext()) {
+                str = reader.next();
+                // System.out.println(str);
+
+                while (96 != str.length()) {
+                    str = "0" + str;
+                }
+
+                if (first) {
+                    writer.write("type ROM_type is array (0 to " + (nmbOfLines - 1)
+                            + ") of std_logic_vector(95 downto 0);" + "\n\n");
+
+                    writer.write(
+                            "constant ROM : ROM_type := (\n   " + 0 + " => \"" + str + "\"," + "\n");
+
+                    // System.out.print(i);
+                    first = false;
+                } else if (i == (nmbOfLines - 1)) {
+
+                    writer.write("   " + i + " => \"" + str + "\");\n");
+
+                } else {
+
+                    writer.write("   " + i + " => \"" + str + "\",\n");
+                    i++;
+                }
+
+            }
+
+            writer.close();
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException f) {
+            f.printStackTrace();
+        }
+
+    }
+
+}
